df935fa3809df20d6e20ec3452b3250119c201cc
medazza <midazza27@gmail.com>
2023-12-09 08:25:25 +0100

    last commit before check

--- models/base_model.py
+++ models/base_model.py
@@ -60,18 +60,3 @@ class BaseModel():
 
         return "[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id, self.__dict__)
-
-
-if __name__ == "__main__":
-    all_objs = models.storage.all()
-    print("-- Reloaded objects --")
-    for obj_id in all_objs.keys():
-        obj = all_objs[obj_id]
-        print(obj)
-
-    print("-- Create a new object --")
-    my_model = BaseModel()
-    my_model.name = "My_First_Model"
-    my_model.my_number = 89
-    my_model.save()
-    print(my_model)
